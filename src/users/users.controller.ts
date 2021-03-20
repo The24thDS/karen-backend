6da2cb9d8424200cebe19975f486151e8db0098c
The24thDS <davidsima@windowslive.com>
2021-03-20 15:53:57 +0200

    Added excluded props to the users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,21 @@
+import User from 'src/types/user';
 import { Controller, Get, Param } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  private readonly excludedProps = ['password'];
+
   constructor(private readonly usersService: UsersService) {}
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    return this.stripExcludedProps(await this.usersService.findOne(id));
+  }
+
+  private stripExcludedProps(res: User) {
+    this.excludedProps.forEach((p) => delete res[p]);
+    return res;
   }
 }

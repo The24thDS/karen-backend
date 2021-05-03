a06d6b6e05e41bcaf1bad8ae263d3466bf9a948b
The24thDS <davidsima@windowslive.com>
2021-05-03 15:23:04 +0300

    Refactored tags service to use the query builder and added Tag interface

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,19 +1,27 @@
-import { Neo4jOrmService } from './../neo4j-orm/neo4j-orm.service';
+import { Neo4jService } from 'nest-neo4j/dist';
 import { Injectable } from '@nestjs/common';
 
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { node, Query } from 'cypher-query-builder';
+import { parseRecord } from 'src/utils/neo4j-utils';
 
 @Injectable()
 export class TagsService {
-  constructor(private readonly neo4jOrm: Neo4jOrmService) {}
+  constructor(private readonly neo4jService: Neo4jService) {}
 
   create(createTagDto: CreateTagDto) {
     return 'This action adds a new tag';
   }
 
-  async findAll(): Promise<any> {
-    return this.neo4jOrm.findAll('Tag');
+  async findAll(): Promise<string[]> {
+    const q = new Query().match([node('tag', 'Tag')]).return('tag');
+    const response = await this.neo4jService.read(q.build());
+    console.log(response.records[0]);
+    const tags: string[] = response.records.map((record) =>
+      parseRecord(record),
+    );
+    return tags;
   }
 
   findOne(id: string) {

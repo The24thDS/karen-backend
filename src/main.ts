f971cdad99048b963464e93ef694ac013d48ec8a
The24thDS <davidsima@windowslive.com>
2021-06-21 20:43:10 +0300

    Added https option

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,31 @@
 require('dotenv').config();
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  let app;
+  if (process.env.SSL === 'true') {
+    const httpsOptions = {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+    };
+    app = await NestFactory.create(AppModule, {
+      httpsOptions,
+    });
+  } else {
+    app = await NestFactory.create(AppModule, { cors: true });
+  }
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(3001);
-  Logger.log('Application started to port 3001');
+
+  await app.listen(process.env.PORT);
+  Logger.log(`Application started to port ${process.env.PORT}`);
 }
 bootstrap();
